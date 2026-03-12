38431933f54323b4952dbde78a807c0c55d9de35
Swetony <swetonyancelmo@gmail.com>
2026-03-12 13:36:52 -0300

    feat: implementa endpoint de retorno de todos oserviços

--- src/main/java/com/swetonyancelmo/agendamentos/controller/ServiceController.java
+++ src/main/java/com/swetonyancelmo/agendamentos/controller/ServiceController.java
@@ -7,10 +7,14 @@ import com.swetonyancelmo.agendamentos.models.Business;
 import com.swetonyancelmo.agendamentos.services.ServiceService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +37,13 @@ public class ServiceController implements ServiceControllerDocs {
     public ResponseEntity<ServiceDto> createService(@RequestBody @Valid CreateServiceDto dto, @AuthenticationPrincipal Business businessLogged) {
         ServiceDto createdService = service.createService(dto, businessLogged);
         return new ResponseEntity<>(createdService, HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    @PreAuthorize("hasAnyRole('ROLE_BUSINESS')")
+    @Override
+    public ResponseEntity<List<ServiceDto>> getAllServices(){
+        return ResponseEntity.ok(service.listAllServices());
     }
 
 }
