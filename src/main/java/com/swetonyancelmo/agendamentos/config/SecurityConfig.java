93bfd7d66fd1add4675e2992606a7e8977a34eda
Swetony <swetonyancelmo@gmail.com>
2026-04-13 23:35:44 -0300

    fix: adiciona nova porta no CORS

--- src/main/java/com/swetonyancelmo/agendamentos/config/SecurityConfig.java
+++ src/main/java/com/swetonyancelmo/agendamentos/config/SecurityConfig.java
@@ -62,6 +62,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
