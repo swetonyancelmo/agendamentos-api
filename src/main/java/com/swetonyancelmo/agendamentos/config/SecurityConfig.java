6a63dc087e03592397c956bc30a2a792e2239b76
Swetony <swetonyancelmo@gmail.com>
2026-04-23 14:57:02 -0300

    fix: resolve bug in cors

--- src/main/java/com/swetonyancelmo/agendamentos/config/SecurityConfig.java
+++ src/main/java/com/swetonyancelmo/agendamentos/config/SecurityConfig.java
@@ -61,8 +61,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://agendamentos-ui.vercel.app"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
